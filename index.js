5c389ade4b6afac194d48b323d0d573b64f0a75f
Mathr81 <112965202+Mathr81@users.noreply.github.com>
2023-07-06 12:33:44 +0000

    modified:   index.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	.env 	node_modules/dotenv/

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+require('dotenv').config()
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'EverytimeBot.aternos.me', // minecraft server ip
-  username: 'mineflayer-bot', // minecraft username
-  // auth: 'microsoft' // for offline mode servers, you can set this to 'offline'
-  port: 25269,                // only set if you need a port that isn't 25565
-  // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-  // password: '12345678'        // set if you want to use password-based auth (may be unreliable)
+  host: process.env.SERVER_IP, // minecraft server ip
+  username: process.env.BOT_USERNAME, // minecraft username
+  port: process.env.SERVER_PORT,
 })
 
 function jump () {
@@ -22,7 +20,7 @@ bot.on('spawn', () => {
         if (Math.random() < 0.5) {
             jump();
         }
-    }, 60000);
+    }, 30000);
 });
 
 function lookAtNearestPlayer () {
